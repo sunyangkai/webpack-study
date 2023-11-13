c7a131f3b59165963152d6e8b73f1a312652067e
sunyangkai <sunyangkai123@qq.com>
2023-11-13 22:05:05 +0800

    差异3

--- scripts/checklog.js
+++ scripts/checklog.js
@@ -1,11 +1,27 @@
 const { execSync } = require('child_process');
 
+function getCurrentBranch() {
+  return execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+}
+
 function checkMerge() {
-  const mergeLog = execSync('git log --merges --pretty=format:"%s"').toString();
-  if (mergeLog.includes('f') && mergeLog.includes('bg')) {
-    console.error('Error: Found a merge commit from "f" branch to "bg" branch.');
-    process.exit(1);
-  }
+  const currentBranch = getCurrentBranch();
+  console.log('currentBranch', currentBranch)
+  const mergeLog = execSync(`git log origin/${currentBranch}..${currentBranch}`).toString(); // --merges --pretty=format:"%s"
+  console.log('mergeLog')
+  console.log(mergeLog)
+  console.log('xx')
+//   process.exit(1);
+
+//   // 检查逻辑q
+//   const hasForbiddenMerge = mergeLog.split('\n').some(commitMessage => {
+//     return commitMessage.startsWith('f') && commitMessage.includes('merge') && commitMessage.includes('bg');
+//   });
+
+//   if (hasForbiddenMerge) {
+//     console.error('Error: Found a forbidden merge commit from "f" branch to "bg" branch.');
+//     process.exit(1);
+//   }
 }
 
 checkMerge();
