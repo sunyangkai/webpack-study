c8944adf1593fc3a9bf168901952697e508f13ff
sunyangkai <sunyangkai123@qq.com>
2023-11-13 18:08:13 +0800

    feat: 完善命令

--- .husky/safe-merge.sh
+++ .husky/safe-merge.sh
@@ -8,7 +8,8 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 # 检查是否允许合并
 if [[ $current_branch == f-* ]] && [[ $target_branch == bg-* ]]; then
     echo "不允许将以 'bg-' 开头的分支合并到以 'f-' 开头的分支"
-    exit 1
+    git merge --abort # 合并操作被触发但不符合你的规则，脚本将自动取消合并过程并退出。这样，合并操作就不会留在一个未完成的状态
+    return 1
 fi
 
 # 如果检查通过，则执行合并
